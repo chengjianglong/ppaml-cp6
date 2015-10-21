1d2d7bd722b1021f35c06a9ab6c8edec215be473
Roddy Collins <roddy.collins@kitware.com>
2015-10-21 17:08:53 -0400

    Add comments to eval script; change MAP computation.
    When computing MAP, stop when when the estimated scores are zero.

--- data_preparation_tools/tools/cp6_eval.py
+++ data_preparation_tools/tools/cp6_eval.py
@@ -32,13 +32,45 @@
 ##
 ## Generate CP6 metrics.
 ##
+## Header lines:
 ##
+## [1] index:         label index
+## [2] label:         label string
+##
+## [3] MAP:           mean average precision
+## [4] n-predictions: number of predictions (non-zero scores) used in computing MAP
+## [5] BER:  balanced error rate
+##
+## [6] n-instances:   number of positive / negative examples of the label
+## [7] n-correct:     number of positive / negative examples correctly labeled
+## [8] %-correct:     computed as [7]/[6]
+##
+## [9]  n-true-pos:        number of true positive examples
+## [10] n-est-pos-correct: number of estimated positive results that were correct
+## [11] pD:                probability of detection; [10] / [9]
+## [12] n-est-pos-wrong:   number of estimated positive results that were incorrect
+##
+## [13] n-true-neg:        number of true negative examples
+## [14] n-est-neg-correct: number of estimated negative results that were correct
+## [15] n-est-neg-wrong:   number of estimated negative results that were incorrect
+##
+## Notes:
+##
+## a) [10] + [12] + [14] + [15] == [6]
+## b) [10] + [15] == [9]
+## c) [12] + [14] == [13]
+##
+## colloquial interpretations:
+## [10]: "We asserted the label is present, and were correct-- we got it!"
+## [12]: "We asserted the label is present, but were wrong-- false positive"
+## [14]: "We asserted the label is absent, and were correct-- true negative"
+## [15]: "We asserted the label is absent, and were wrong-- we missed it."
 ##
 
 import sys
 from cp6.utilities.util import Util
 from cp6.tables.image_table import ImageTable
-from cp6.utilities.image_edge import ImageEdge
+from cp6.utilities.image_table_entry import ImageTableEntry
 from cp6.tables.label_table import LabelTable
 
 if len(sys.argv) != 5:
@@ -61,7 +93,6 @@ n_fields = 0
 with open( sys.argv[3] ) as f:
     n_fields = len(f.readline().split())
 
-print "Found ",n_fields
 if n_fields == nLabels + 1:
     # attempt to read as a scoring-only pseudo-image-table
     computed_it = ImageTable()
@@ -114,7 +145,7 @@ sys.stderr.write('Unscored:   %d\n' % image_id_state_map.values().count( 0x01 ))
 sys.stderr.write('Extraneous: %d\n' % image_id_state_map.values().count( 0x02 ))
 
 # emit CSV header
-sys.stdout.write('"index","label","MAP","BER","n-instances","n-correct","%-correct","n-true-pos","n-computed-pos","n-true-neg","n-computed-neg"\n')
+sys.stdout.write('"index","label","MAP","n-predictions","BER","n-instances","n-correct","%-correct","n-true-pos","n-est-pos-correct","pD","n-est-pos-wrong","n-true-neg","n-est-neg-correct","n-est-neg-wrong"\n')
 
 for i in range(0, nLabels):
     # create mapping from computed score to correct/incorrect
@@ -125,7 +156,7 @@ for i in range(0, nLabels):
     score_map = dict()  # key: image_id, val: score
     correct_map = dict() # key: image_id, val: True if correct, False if not
 
-    (n_instances, n_true_pos, n_computed_pos, n_true_neg, n_computed_neg) = (0,0,0,0,0)
+    (n_instances, n_true_pos, n_computed_pos_correct, n_computed_pos_wrong, n_true_neg, n_computed_neg_correct, n_computed_neg_wrong) = (0,0,0,0,0,0,0)
 
     for (image_id, true_image_entry) in true_it.entries.iteritems():
         if image_id_state_map[ image_id ] != 0x03:
@@ -138,16 +169,21 @@ for i in range(0, nLabels):
 
         n_instances += 1
         score_map[ image_id ] = s
+
         if  r == 1:
             n_true_pos += 1
             result = (s >= t)
             if result:
-                n_computed_pos += 1
+                n_computed_pos_correct += 1
+            else:
+                n_computed_neg_wrong += 1
         elif r == 0:
             n_true_neg += 1
             result = (s < t)
             if result:
-                n_computed_neg += 1
+                n_computed_neg_correct += 1
+            else:
+                n_computed_pos_wrong += 1
         else:
             raise AssertionError( 'Image %d label %d: unexpected label vector value %f\n' % (image_id, i, r))
 
@@ -159,21 +195,25 @@ for i in range(0, nLabels):
 
     (n_predictions, n_correct_so_far, sum_ap) = (0, 0, 0)
     for (image_id, score) in sorted( score_map.items(), key=lambda x:-x[1]):
-        n_predictions += 1
-        if correct_map[ image_id ]:
-            n_correct_so_far += 1
-        this_ap = n_correct_so_far / n_predictions
-        sum_ap += this_ap
+        # stop computing once we run out of non-zero responses
+        if score > 0:
+            n_predictions += 1
+            if correct_map[ image_id ]:
+                n_correct_so_far += 1
+            this_ap = 1.0 * n_correct_so_far / n_predictions
+            sum_ap += this_ap
+
+    meanavgprec = 1.0*sum_ap / n_predictions if n_predictions > 0 else -1
 
     n_correct_total = correct_map.values().count( True )
-    if n_correct_total != n_correct_so_far:
-        raise AssertionError('Logic error: label %d total correct %d vs n-so-far %d\n' % (i, n_correct_total, n_correct_so_far))
 
     sys.stdout.write('%d,%s,' % (i, lt.id2label[i]))
-    sys.stdout.write('%0.5f,' % (sum_ap / n_instances))
-    ber_pos = (n_computed_pos / n_true_pos) / n_true_neg
-    ber_neg = (n_computed_neg / n_true_neg) / n_true_pos
+    sys.stdout.write('%0.5f,%d,' % (meanavgprec, n_predictions) )
+    ber_pos = (1.0 * n_computed_pos_correct / n_true_pos) / n_true_neg
+    ber_neg = (1.0 * n_computed_neg_correct / n_true_neg) / n_true_pos
     ber = (ber_pos + ber_neg) / 2.0
     sys.stdout.write('%0.5f,' % ber )
     sys.stdout.write('%d,%d,%0.5f,' % (n_instances, n_correct_total, 1.0*n_correct_total/n_instances))
-    sys.stdout.write('%d,%d,%d,%d\n' % (n_true_pos, n_computed_pos, n_true_neg, n_computed_neg))
+    sys.stdout.write('%d,%d,%0.5f,%d,' % (n_true_pos, n_computed_pos_correct, 1.0*n_computed_pos_correct / n_true_pos,n_computed_pos_wrong ))
+    sys.stdout.write('%d,%d,%d\n' % (n_true_neg, n_computed_neg_correct,n_computed_neg_wrong))
+
