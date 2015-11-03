b3483e43f8d7a2e87cce236bb26cf45920424d7d
eswears <eran.swears@kitwaer.com>
2015-11-03 16:46:38 -0500

    Changed f_load_edges flag to zero in crf_config_file.m

--- crf_baseline_scripts/crf_config_file.m
+++ crf_baseline_scripts/crf_config_file.m
@@ -5,7 +5,7 @@
 
 function [f_load_edges, f_plot, marbl_path]= crf_config_file()
 
-f_load_edges= 1; %0=load previous calculate edge data, 1= calculate group and word edge data
+f_load_edges= 0; %0=load previous calculate edge data, 1= calculate group and word edge data
 f_plot      = 0; %Plot ROC/PR curves
 marbl_path  = ['\home\eswears\Projects\code\marbl']; %Path to marbl toolbox, i.e. parent folder for learn_crf.exe
 
