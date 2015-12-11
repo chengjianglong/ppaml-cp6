45cad99ff9c7f7053decce760b59186a10f77ee0
Michael Hueschen <mhueschen@galois.com>
2015-12-10 18:04:12 -0800

    eval.sh - evaluate actual data, not random prior

--- data_preparation_tools/eval.sh
+++ data_preparation_tools/eval.sh
@@ -13,4 +13,4 @@ run_out=$1
 eval_in=$2
 eval_output_dir=$3
 
-python $ABSOLUTE_PATH/tools/cp6_eval.py --lt $eval_in/etc/label_table.txt --tt $eval_in/testing/image_table.txt --ct $run_out/testing/classified_labels.txt --tht $run_out/testing/label_threshold.txt --r p &> $eval_output_dir/eval.log
+python $ABSOLUTE_PATH/tools/cp6_eval.py --lt $eval_in/etc/label_table.txt --tt $eval_in/testing/image_table.txt --ct $run_out/testing/classified_labels.txt --tht $run_out/testing/label_threshold.txt &> $eval_output_dir/eval.log
